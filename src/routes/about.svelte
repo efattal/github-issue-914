3afb5e77d70c651c506a56b60792b2292e4f3a13
e.fattal <e.fattal@lectra.com>
2019-09-25 14:40:47 +0200

    Misc

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,3 +1,15 @@
+<script context="module">
+	export async function preload({params, query}) {
+		return {
+			foo: query.foo
+		}
+	}
+</script>
+
+<script>
+	export let foo;
+</script>
+
 <svelte:head>
 	<title>About</title>
 </svelte:head>
@@ -5,3 +17,5 @@
 <h1>About this site</h1>
 
 <p>This is the 'about' page. There's not much here.</p>
+
+<p>Foo: {foo}</p>
